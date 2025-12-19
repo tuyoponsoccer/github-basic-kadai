36506de560bcb1c73af9f98b7da0f9a9505384e6
tuyoponsoccer <tuyoponsoccer@gmail.com>
2025-12-19 23:47:51 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+// 現在の日付を取得
+const today = new Date();
+
+// 年・月・日をそれぞれ取得
+const year = today.getFullYear();      // 年
+const month = today.getMonth() + 1;    // 月（※0始まりなので +1）
+const day = today.getDate();           // 日
+
+// 文字列として出力
+console.log("年: " + year);
+console.log("月: " + month);
+console.log("日: " + day);
